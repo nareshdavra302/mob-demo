fb05ef3b794634aef456ccf96a2430e992da4a3b
Naresh Davra <naresh.davra@gmail.com>
2021-05-19 13:44:48 +0530

    bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,30 +8,28 @@ const IndexPage = () => {
 
 
   const [txnArrayStr, setTxnArrayStr] = useState('');
-  const [txnArrayIn, setTxnArrayIn] = useState([]);
   const [encodedTextIn, setEncodedTextIn] = useState('');
   const [plainTextOut, setPlainTextOut] = useState('');
 
   const onEncode = (data) => {
     setPlainTextIn(data);
     // if (!data) return;
-    onEncodeOut(data)
+    const d = encodeData(data)
+    onEncodeOut(d)
   }
 
   const onEncodeOut = (data) => {
-    const d = encodeData(data);
-    setEncodedTextOut(d);
+
+    setEncodedTextOut(data);
     
-    const txns = converttoTxnArray(d);
+    const txns = converttoTxnArray(data);
     setTxnArrayOut(txns);
   }
 
   const onDecodeTxn = (txnArrStr) => {
-    console.log('sd',)
-
     setTxnArrayStr(txnArrStr);
     const txnArray = txnArrStr.split('\n')
-    setTxnArrayIn(txnArray);
+    // setTxnArrayIn(txnArray);
 
     // if (!txnArray) return;
     const encStr = convertEncodedString(txnArray)
@@ -51,17 +49,17 @@ const IndexPage = () => {
         <div className="row sm:flex">
           <div className="col sm:w-1/2">
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Plain Text</h3></div>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Plain Text Input</h3></div>
               <textarea className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={plainTextIn} onChange={e => onEncode(e.target.value)} ></textarea>
             </div>
             <br />
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Ecoded Text</h3></div>
-              <textarea className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={encodedTextOut} onChange={e => onEncode(e.target.value)}></textarea>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Encoded Text</h3></div>
+              <textarea className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={encodedTextOut} onChange={e => onEncodeOut(e.target.value)}></textarea>
             </div>
             <br />
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Transactions</h3></div>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Transactions Output</h3></div>
               <div className="text-grey-darkest flex-1 p-2 m-1 bg-transparent">{txnArrayOut.map((tx => <ul key={tx}>{tx}</ul>))}</div>
             </div>
 
@@ -69,17 +67,17 @@ const IndexPage = () => {
 
           <div className="col mt-8 sm:ml-8 sm:mt-0 sm:w-1/2">
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Transactions</h3></div>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Transactions Input</h3></div>
               <textarea className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={txnArrayStr} onChange={e => onDecodeTxn(e.target.value)}></textarea>
             </div>
             <br />
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Ecoded Text</h3></div>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Encoded Text</h3></div>
               <textarea className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={encodedTextIn} onChange={e => onDecode(e.target.value)}></textarea>
             </div>
             <br />
             <div className="box border rounded flex flex-col shadow bg-white">
-              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Plain Text</h3></div>
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b"><h3 className="text-sm text-grey-darker font-medium">Plain Text Output</h3></div>
               <textarea placeholder="hey" className="text-grey-darkest flex-1 p-2 m-1 bg-transparent" value={plainTextOut} onChange={e => setPlainTextOut(e.target.value)} ></textarea>
             </div>
           </div>
